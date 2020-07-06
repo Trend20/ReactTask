9e212a35dcde3dca1c4253ad0ab13bc94f4815c3
Trend20 <enockomondi305@gmail.com>
2020-07-06 19:27:13 -0400

    card

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Banner from './components/Banner/Banner';
+import Paper from './components/Paper/Paper';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
+      <Paper />
     </div>
   );
 }
