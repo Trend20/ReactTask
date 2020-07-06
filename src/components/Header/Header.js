0d98c5c0fa2f4ab4109f9457c5f2c7c540daf4dd
Trend20 <enockomondi305@gmail.com>
2020-07-06 17:09:37 -0400

    header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+ 
+class Header extends Component {
+    render() { 
+        return (
+            <div className="header">
+                <header>
+                    <div className="navigation">
+                        
+                    </div>
+                </header>
+            </div>
+        );
+    }
+}
+ 
+export default Header;
