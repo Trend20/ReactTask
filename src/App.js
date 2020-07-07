3eec77094140f8b674c2290013dfa0715d2a3f48
Trend20 <enockomondi305@gmail.com>
2020-07-07 08:07:29 -0400

    nav

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Banner from './components/Banner/Banner';
-import Paper from './components/Paper/Paper';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Banner />
-      <Paper />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,75 +1,36 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
-import Logo from '../../assets/img/brotherhood-global-foundation1.png';
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './Header.css';
- 
-class Header extends Component {
-    render() { 
-        return (
-            <div className="heading">
-                <header>
-                    <div className="logo">
-                        <img src={Logo} />
-                    </div>
-                    {/* <div className="navigation">
-                        <Router>
-                            <div>
-                                <nav className="main-navbar">
-                                    <ul>
-                                        <li>
-                                            <Link to="/">Home</Link>
-                                        </li>
-                                        <li>
-                                            <Link to="/about">About</Link>
-                                        </li>
-                                        <li>
-                                            <Link to="/blogs">Blogs</Link>
-                                        </li>
-                                        <li>
-                                            <Link to="/contact">Contact</Link>
-                                        </li>
-                                    </ul>
-                                </nav>
-                                <Switch>
-                                    <Route path="/about">
-                                        About
-                                    </Route>
-                                    <Route path="/blogs">
-                                        Blogs
-                                    </Route>
-                                    <Route path="/contact">
-                                        Contact
-                                    </Route>
-                                    <Route path="/">
-                                        Home
-                                    </Route>
-                                    
-                                </Switch>
-                            </div>
-                        </Router>
-                    </div> */}
-                    <div className="links">
-                        <nav>
-                            <ul>
-                                <li>
-                                    <a href="#">Home</a>
-                                </li>
-                                <li>
-                                    <a href="#">About</a>
-                                </li>
-                                <li>
-                                    <a href="#">Blogs</a>
-                                </li>
-                                <li>
-                                    <a href="#">Contact</a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
-                </header>
+import Logo from '../../assets/img/brotherhood-global-foundation.png';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <header>
+            <div className="logo">
+                <img src={Logo} />
             </div>
-        );
-    }
+            <div className="navigation">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-light">
+                   <ul className="nav">
+                       <li className="nav-link">
+                           <a href="#" className="nav-item">Home</a>
+                       </li>
+                       <li className="nav-link">
+                           <a href="#" className="nav-item">About</a>
+                       </li>
+                       <li className="nav-link">
+                           <a href="#" className="nav-item">Blogs</a>
+                       </li>
+                       <li className="nav-link">
+                           <a href="#" className="nav-item">Contacts</a>
+                       </li>
+                   </ul>
+                </nav>
+            </div>
+        </header>
+      </div>
+    );
+  }
 }
- 
-export default Header;
