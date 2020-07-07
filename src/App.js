2b40ce90feab8118c92c37f16d20a13cee176a95
Trend20 <enockomondi305@gmail.com>
2020-07-07 09:50:22 -0400

    cards

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Jumbotron from './components/Jumbotron/Jumbotron';
+import Cards from './components/Cards/Cards';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Jumbotron />
+      <Cards />
     </div>
   );
 }
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import Mission from '../../assets/img/mission.png'
+import Vission from '../../assets/img/vission.png'
+import Moto from '../../assets/img/moto.png'
+import './Cards.css';
+ 
+class Cards extends Component {
+    render() { 
+        return (
+            <div className="container" style={{height: '50vh'}}>
+                <div className="row">
+                 <div className="col-sm-4">
+                 <div className="card card-details" style={{
+                     width: '23rem',
+                     height: '40vh'
+                     }}>
+                    <img src={Mission} className="card-img" style={{
+                         width: '7vw',
+                         marginLeft:'10px'
+
+
+                    }} />
+                    <div className="card-body">
+                        <h1 className="card-title">Mission</h1>
+                        <p className="card-text">To eradicate hunger and poverty.</p>
+                    </div>
+                </div>
+                 </div>
+                <div className="col-sm-4">
+                <div className="card card-details" style={{
+                    width: '23rem',
+                    height: '40vh'
+                    }}>
+                    <img src={Vission} style={{
+                         width: '7vw',
+                         marginLeft:'10px'
+                    }} />
+                    <div className="card-body">
+                        <h1 className="card-title">Vission</h1>
+                        <p className="card-text">A world full of people devoid of miseries and pain.</p>
+                    </div>
+                </div>
+                </div>
+                <div className="col-sm-4">
+                <div className="card card-details" style={{
+                    width: '23rem',
+                    height: '40vh'
+                    }}>
+                    <img src={Moto} style={{
+                        width: '7vw',
+                        marginLeft:'10px'
+                    }}  />
+                    <div className="card-body">
+                        <h1 className="card-title">Moto</h1>
+                        <p className="card-text">We rise by lifting others.</p>
+                    </div>
+                </div>
+                </div>
+                </div>
+            </div>
+        );
+    }
+}
+ 
+export default Cards;
