1b00eb19444a8d4085a9f00dd10ef3564661b502
Trend20 <enockomondi305@gmail.com>
2020-07-08 17:42:21 -0400

    help desc

--- src/components/help/Help.js
+++ src/components/help/Help.js
@@ -4,13 +4,15 @@ class Help extends Component {
     render() { 
         return (
             <div className="container-fluid">
-                <div className="help-contents">
-                    <h3>A Small Help</h3>
+                <div className="help-header">
+                    <h3 className="text-dark text-center">A Small Help</h3>
                     <span></span>
+                    <div className="help-desc">
                     <p>Brotherhood global foundation Trust has created a revolution among youngsters, which 
                     not only based on ethics but also added value and believing in 
                     our culture and tradition also.
                     </p>
+                    </div>
                 </div>
 
             </div>
