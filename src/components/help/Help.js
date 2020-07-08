02df0cf6ea1b5f546badb6054884633bba0c61db
Trend20 <enockomondi305@gmail.com>
2020-07-08 17:48:14 -0400

    span styles

--- src/components/help/Help.js
+++ src/components/help/Help.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
+import './Help.css';
  
 class Help extends Component {
     render() { 
         return (
             <div className="container-fluid">
                 <div className="help-header">
-                    <h3 className="text-dark text-center">A Small Help</h3>
-                    <span></span>
-                    <div className="help-desc">
-                    <p>Brotherhood global foundation Trust has created a revolution among youngsters, which 
-                    not only based on ethics but also added value and believing in 
-                    our culture and tradition also.
-                    </p>
-                    </div>
+                     <h3 className="text-dark text-center">A Small Help</h3>
+                     <span></span>
                 </div>
-
+                    <div className="help-desc">
+                        <p>Brotherhood global foundation Trust has created a revolution among youngsters, which 
+                           not only based on ethics but also added value and believing in 
+                           our culture and tradition also.
+                        </p>
+                    </div>
             </div>
         );
     }
