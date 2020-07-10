a3c8639990d3d66d2214c119b944a10d04d6cfb1
Trend20 <enockomondi305@gmail.com>
2020-07-10 18:29:43 -0400

    image dir

---
+++ src/components/help/cardimage/CardImage.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+export default class CardImage extends Component {
+  render() {
+    return (
+        <div className="container">
+        <div className="row">
+            <div className="col-sm-4">
+                <div className="card" style={{
+                    width: '23rem',
+                    height: '40vh'
+                    }}>
+                   <img  src={cardImage} className="card-image-top"/>
+                   <div className="card-body">
+                       <h1 className="card-title">COVID-19 Relief Services</h1>
+                       <p className="card-text">In Nairobi Kenya</p>
+                   </div>
+                </div>
+            </div>
+            <div className="col-sm-4">
+                <div className="card" style={{width: '18rem'}}>
+                   <img  src={cardImage} className="card-image-top"/>
+                   <div className="card-body">
+                       <h1 className="card-title">COVID-19 Relief Services</h1>
+                       <p className="card-text">In Nairobi Kenya</p>
+                   </div>
+                </div>
+            </div>
+            <div className="col-sm-4">
+                <div className="card" style={{width: '18rem'}}>
+                   <img  src={cardImage} className="card-image-top"/>
+                   <div className="card-body">
+                       <h1 className="card-title">COVID-19 Relief Services</h1>
+                       <p className="card-text">In Nairobi Kenya</p>
+                   </div>
+                </div>
+            </div>
+            <div className="col-sm-4">
+                <div className="card" style={{width: '18rem'}}>
+                   <img  src={cardImage} className="card-image-top"/>
+                   <div className="card-body">
+                       <h1 className="card-title">COVID-19 Relief Services</h1>
+                       <p className="card-text">In Nairobi Kenya</p>
+                   </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
+  }
+}
