dc7ec9e0ee737c1c27a5271816071b4d61b3d218
Trend20 <enockomondi305@gmail.com>
2020-07-11 18:26:02 -0400

    starter

--- src/components/help/cardimage/CardImage.js
+++ src/components/help/cardimage/CardImage.js
@@ -1,68 +1,13 @@
 import React, { Component } from 'react';
-import './CardImage.css';
-import cardImage from '../../../assets/img/blog1.jpeg';
-
-export default class CardImage extends Component {
-  render() {
-    return (
-        <div className="container">
-        <div className="row">
-            <div className="col-sm-4">
-                <div className="card" style={{
-                     width: '18rem',
-                     height:'30vh'
-                    }}>
-                   <img  src={cardImage} 
-                   className="card-image-top"
-                   style={{height: '25vh'}}
-                   />
-                   <div className="card-body">
-                       <h1 className="card-title">COVID-19 Relief Services</h1>
-                       <p className="card-text">In Nairobi Kenya</p>
-                   </div>
-                </div>
+ 
+class CardImage extends Component {
+    render() { 
+        return (
+            <div>
+                CardImage
             </div>
-            <div className="col-sm-4">
-                <div className="card" style={{
-                    width: '18rem',
-                    height:'30vh'
-                    }}>
-                   <img  src={cardImage} 
-                   className="card-image-top"
-                       style={{height: '25vh'}}
-                   />
-                   <div className="card-body">
-                       <h1 className="card-title">COVID-19 Relief Services</h1>
-                       <p className="card-text">In Nairobi Kenya</p>
-                   </div>
-                </div>
-            </div>
-            <div className="col-sm-4">
-                <div className="card" style={{width: '18rem'}}>
-                   <img  src={cardImage} 
-                   className="card-image-top"
-                   style={{height: '25vh'}}
-                   />
-                   <div className="card-body">
-                       <h1 className="card-title">COVID-19 Relief Services</h1>
-                       <p className="card-text">In Nairobi Kenya</p>
-                   </div>
-                </div>
-            </div>
-            <div className="col-sm-4">
-                <div className="card" style={{width: '18rem'}}>
-                   <img  src={cardImage} 
-                   className="card-image-top"
-                   style={{height: '25vh'}}
-                   />
-                   <div className="card-body">
-                       <h1 className="card-title">COVID-19 Relief Services</h1>
-                       <p className="card-text">In Nairobi Kenya</p>
-                   </div>
-                </div>
-            </div>
-        </div>
-        </div>
-    );
-  }
+        );
+    }
 }
+ 
+export default CardImage;
