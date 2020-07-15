d4c4f84ec5ea446bb5b2343aae4ddd6a3dad4e0c
Trend20 <enockomondi305@gmail.com>
2020-07-15 16:10:10 -0400

    carousel-inner

--- src/components/chevron/Caurosel.js
+++ src/components/chevron/Caurosel.js
@@ -11,6 +11,17 @@ class Carousel extends Component {
                       <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                       <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
                    </ol>
+                   <div className="carousel-inner">
+                      <div className="carousel-item active">
+                         <img className="d-block w-100" src="..." alt="First slide" />
+                      </div>
+                      <div className="carousel-item">
+                         <img className="d-block w-100" src="..." alt="Second slide" />
+                      </div>
+                      <div className="carousel-item">
+                          <img className="d-block w-100" src="..." alt="Third slide" />
+                     </div>
+                   </div>
                </div>
             </div>
         );
