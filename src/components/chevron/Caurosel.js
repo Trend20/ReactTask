d4a368a3d3374f279d2cef96910dc22847499e7c
Trend20 <enockomondi305@gmail.com>
2020-07-15 16:06:37 -0400

    ordered list

--- src/components/chevron/Caurosel.js
+++ src/components/chevron/Caurosel.js
@@ -5,9 +5,13 @@ class Carousel extends Component {
     render() { 
         return (
             <div className="container">
-                <div></div>
-                <img src={}/>
-                <div></div>
+               <div className="carousel slide" id="carouselExampleIndicators" dataRide="carousel">
+                   <ol className="carousel-indicators">
+                      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                   </ol>
+               </div>
             </div>
         );
     }
