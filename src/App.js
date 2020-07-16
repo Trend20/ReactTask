dadb01c4d8ed966885c93fa314e61342c941dada
Trend20 <enockomondi305@gmail.com>
2020-07-16 15:35:31 -0400

    banner-render

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Help from './components/help/Help';
 import Grid from './components/grid/Grid';
 import Team from './components/team/Team';
 import Carousel from './components/carousel/Carousel';
+import Banner from './components/bunner/Bunner';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
       <Grid />
       <Team />
       <Carousel />
+      <Banner />
     </div>
   );
 }
