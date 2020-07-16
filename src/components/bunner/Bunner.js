d620816edce8673c4dd49a8ceb5676f4390785ff
Trend20 <enockomondi305@gmail.com>
2020-07-16 15:46:28 -0400

    banner-style

--- src/components/bunner/Bunner.js
+++ src/components/bunner/Bunner.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import Index1 from '../../assets/img/index1.png';
 import Index2 from '../../assets/img/index2.png'
 import Index3 from '../../assets/img/index3.png'
-import './Banner.css';
+import './Bunner.css';
 
 class Bunner extends Component {
   render() {
     return (
-      <div className="container">
+      <div className="banner-container">
         <div className="first-image-container">
             <img src={Index1} />
             <h3>Ration kits distributed</h3>
@@ -27,4 +27,4 @@ class Bunner extends Component {
     );
   }
 }
-export default Banner;
+export default Bunner;
