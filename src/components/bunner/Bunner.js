6fb18cc83fc494733d0910c4f0cbfd485dad4f61
Trend20 <enockomondi305@gmail.com>
2020-07-16 17:23:29 -0400

    index-style

--- src/components/bunner/Bunner.js
+++ src/components/bunner/Bunner.js
@@ -10,17 +10,17 @@ class Bunner extends Component {
     return (
       <div className="banner-container">
         <div className="first-image-container">
-            <img src={Index1} className="index"/>
+           <div className="index"> <img src={Index1} /></div>
             <h3>Ration kits distributed</h3>
             <h1>5900</h1>
         </div>
         <div className="second-image-container">
-            <img src={Index2} className="index"/>
+        <div className="index"> <img src={Index2} /></div>
             <h3>Cash help extended</h3>
             <h1>Rs 240654</h1>
         </div>
         <div className="third-image-container">
-            <img src={Index3} className="index"/>
+        <div className="index"> <img src={Index3} /></div>
             <h3>People provided help</h3>
             <h1>3565</h1>
         </div>
