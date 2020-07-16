94ff1d3db719c360cb9a96fc66b0e1465fb48463
Trend20 <enockomondi305@gmail.com>
2020-07-16 15:26:14 -0400

    banner

---
+++ src/components/bunner/Bunner.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Bunner extends Component {
+  render() {
+    return (
+      <div>
+        <div></div>
+      </div>
+    );
+  }
+}
+export default Banner;
