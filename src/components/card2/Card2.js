54c2e029e07f70f2da9d7627bb8508bd1f8fdd20
Trend20 <enockomondi305@gmail.com>
2020-07-17 11:38:02 -0400

    img-style

--- src/components/card2/Card2.js
+++ src/components/card2/Card2.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import blogImage from '../../assets/img/blog3.jpeg';
 import './Card2.css';
  
 class Card2 extends Component {
@@ -11,6 +12,47 @@ class Card2 extends Component {
                    <span></span>
                    <p>Making a difference by extending a helping hand 
                    for a social cause is like prestidigitating the magic of highest prestige.</p>
+               </div>
+               <div className="container">
+               <div className="card" style={{
+                   width:'30vw',
+                   height:'100vh'
+                   }}>
+                   <img  src={blogImage} 
+                       style={{
+                           height:'40vh',
+                       }}
+                   />
+                   <div className="card-body">
+                       <h1 className="card-title">A helping hand</h1>
+                       <p className="card-text">A helping hand is a critical need ...</p>
+                       <a href="#">READ MORE</a>
+                   </div>
+               </div>
+               <div className="card" style={{width:'18rem'}}>
+                   <img  src={blogImage} 
+                       style={{
+                           height:'40vh',
+                       }}
+                   />
+                   <div className="card-body">
+                       <h1 className="card-title">A helping hand</h1>
+                       <p className="card-text">A helping hand is a critical need ...</p>
+                       <a href="#">READ MORE</a>
+                   </div>
+               </div>
+               <div className="card" style={{width:'18rem'}}>
+                   <img  src={blogImage} 
+                       style={{
+                           height:'40vh',
+                       }}
+                   />
+                   <div className="card-body">
+                       <h1 className="card-title">A helping hand</h1>
+                       <p className="card-text">A helping hand is a critical need ...</p>
+                       <a href="#">READ MORE</a>
+                   </div>
+               </div>
                </div>
             </div>
         );
