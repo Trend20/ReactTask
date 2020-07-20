ecdbb35c635e898bc23cb2fae908c48ecfb4493c
Trend20 <enockomondi305@gmail.com>
2020-07-20 17:52:00 -0400

    works

---
+++ src/components/works/Works.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Work.css';
+ 
+class Works extends Component {
+    render() { 
+        return (
+            <div>
+                Works
+            </div>
+        );
+    }
+}
+ 
+export default Works;
