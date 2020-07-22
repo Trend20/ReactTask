830deca30819c7ac546e00824437de3e100215b8
Trend20 <enockomondi305@gmail.com>
2020-07-22 16:24:06 -0400

    p style

--- src/components/works/Works.js
+++ src/components/works/Works.js
@@ -8,10 +8,11 @@ class Works extends Component {
                 <div className="works-heading">
                     <h3>Major Works</h3>
                     <span className="span3"></span>
-                    <p></p>Our major work based on Child Education, Child health, 
+                    <p className="work-text">Our major work based on Child Education, Child health, 
                            Save Girl Child, Girl Education, Women health care and education, 
                            Employment, Training, poor rehabilitation,
                            Food & Nutrition, plantation, Serving old orphan people, public awareness etc.
+                    </p>
                 </div>
             </div>
         );
